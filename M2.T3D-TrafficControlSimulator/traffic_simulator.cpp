050b84cdc08002df861d5117d12e13d724817c2e
2410994805 <jahan4805.se24@chitkara.edu.in>
2026-05-14 11:45:15 +0530

    Update traffic_simulator.cpp

--- M2.T3D-TrafficControlSimulator/traffic_simulator.cpp
+++ M2.T3D-TrafficControlSimulator/traffic_simulator.cpp
@@ -1,6 +1,6 @@
 // FILE: traffic_simulator.cpp
 // TASK: M2.T3D - Traffic Control Simulator (Distinction)
-// UNIT: SIT315 Programming Paradigms, Trimester 4, 2024-25
+// UNIT: SIT315 Programming Paradigms, Semester-4
 // AUTHOR: Jahan Garg | Roll: 2410994805
 //
 // Implements a multi-threaded traffic signal monitoring system using the
